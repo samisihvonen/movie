b3904b30b26a23311f75574f059dc5edf8a31edf
sami sihvonen <sami@liveeye.fi>
2023-02-03 10:53:28 +0200

    first commit

---
+++ src/main/java/dev/sam/server/movies/MovieService.java
@@ -0,0 +1,19 @@
+package dev.sam.server.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+    public List<Movie> allMovies(){
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdb) {
+        return repository.findMovieByImdbId(imdb);
+    }
+}
